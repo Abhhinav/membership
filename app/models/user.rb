3b168557a79ce39b588b80eb013ab732b83a6b2d
abhhinav <abhinavkmr86@gmail.com>
2020-09-24 14:44:37 +0530

    jwt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+
+  def generate_jwt
+    JWT.encode({
+      id: id,
+      exp: 60.days.from_now.to_i},
+      Rails.applications.secrets.secret_key_base)
+  end
 end
